688416b7ac585efa79f04e5d447e79c42d6a8641
Javohir708 <joxauzking@gamil.com>
2025-01-24 18:42:41 +0500

    struktura tayyorlanmoqda

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-500 '>Hello world</div>
+    </>
+  )
+}
+
+export default App
